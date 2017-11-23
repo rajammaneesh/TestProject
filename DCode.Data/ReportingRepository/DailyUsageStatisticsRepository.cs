5f1fdb4bbd6a8d1d1d9b5315305f01f6d0f6945e
Ritwik Sen <risen@deloitte.com>
2017-11-23 11:06:06 +0530

    Add Daily Statistics Count

---
+++ DCode.Data/ReportingRepository/DailyUsageStatisticsRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using DCode.Data.DbContexts;
+using DCode.Data.Repository;
+using System.Linq;
+
+namespace DCode.Data.ReportingRepository
+{
+    public class DailyUsageStatisticsRepository :
+        Repository<daily_usage_statistics>,
+        IDailyUsageStatisticsRepository
+    {
+        public IEnumerable<daily_usage_statistics> GetDailyStatisticsFor(DateTime date)
+        {
+            return GetDailyStatisticsHistory()
+                 .Where(x => x.date.Date == date.Date);
+        }
+
+        public IEnumerable<daily_usage_statistics> GetDailyStatisticsHistory()
+        {
+            return Context.Set<daily_usage_statistics>();
+        }
+    }
+}
